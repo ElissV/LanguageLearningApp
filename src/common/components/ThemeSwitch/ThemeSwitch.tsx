081f267b180b9f2353bfc1db2e23e034474c6233
Elis Vassiltsova <elis.vassiltsova@gmail.com>
2023-11-14 20:30:41 +0200

    Dropdown menu layout and styles changed

--- src/common/components/ThemeSwitch/ThemeSwitch.tsx
+++ src/common/components/ThemeSwitch/ThemeSwitch.tsx
@@ -2,13 +2,39 @@ import Switch from '@mui/material/Switch';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import ModeNightIcon from '@mui/icons-material/ModeNight';
 import styles from "./styles.module.scss";
+import { createTheme, ThemeProvider } from '@mui/material';
 
 export function ThemeSwitch() {
+
+    const theme = createTheme({
+        components: {
+            MuiSwitch: {
+                styleOverrides: {
+                    colorPrimary: {
+                        "&.Mui-checked": {
+                            color: "#fff",
+                        }
+                    },
+                    track: {
+                        backgroundColor: "#375705",
+                        opacity: 0.6,
+                        ".Mui-checked.Mui-checked + &": {
+                          backgroundColor: "#375705",
+                          opacity: 0.6
+                        }
+                    }
+                }
+            }
+        }
+    });
+
     return (
         <>
             <div className={styles.MainContainer}>
                 <LightModeIcon />
-                <Switch />
+                <ThemeProvider theme={theme}>
+                    <Switch />
+                </ThemeProvider>
                 <ModeNightIcon />
             </div>
         </>
