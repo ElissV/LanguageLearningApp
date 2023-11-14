66e7bb34f1751417f5781f976ac2b89af4be3483
Elis Vassiltsova <elis.vassiltsova@gmail.com>
2023-11-14 15:32:09 +0200

    Description section layout changes

--- src/pages/Main/DescriptiveSection/DescriptiveSection.tsx
+++ src/pages/Main/DescriptiveSection/DescriptiveSection.tsx
@@ -3,16 +3,18 @@ import styles from "./DescriptiveSection.module.scss";
 
 function DescriptiveSection() {
     return (
-        <section>
-            <div className={styles.TextContainer}>
-                <p>Improve your vocabulary.</p>
-                <p>Start today.</p>
-                <p>Don't be lazy.</p>
-                <p>Memorize words that you specify yourself.</p>
-                <p>Free of charge!</p>
-            </div>
-            <div className={styles.CardContainer}>
-                <Card />
+        <section className={styles.SectionContainer}>
+            <div className={styles.SectionInnerContainer}>
+                <div className={styles.TextContainer}>
+                    <p>Improve your vocabulary.</p>
+                    <p>Start today.</p>
+                    <p>Don't be lazy.</p>
+                    <p>Memorize words that you specify yourself.</p>
+                    <p>Free of charge!</p>
+                </div>
+                <div className={styles.CardContainer}>
+                    <Card />
+                </div>
             </div>
         </section>
     );
